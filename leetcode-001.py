8fda96475d21c27dac4c0ec238e7946b3a471e99
lixiang <lixiang6632@outlook.com>
2019-04-01 17:13:42 +0800

    add leetcode_001

---
+++ leetcode-001.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: 'List[int]', target: 'int') -> 'List[int]':
+        hashmap = {}
+        for index, num in enumerate(nums):
+            target_num = target - num
+            if target_num in hashmap:
+                return [hashmap[target_num], index]
+            hashmap[num] = index
+        return None
